1e2016e0b302df0821ee2510c54418f911659b27
yuchen <yangyuchen428@gmail.com>
2019-08-09 00:38:52 +0800

    fix minor bugs.

--- static/js/import_export.js
+++ static/js/import_export.js
@@ -1,5 +1,5 @@
 function export_word(qid, filename) {
-    fetch("/resource/.sys/quest/" + qid + "/article.html").then(function (response) {
+    fetch("/resource/.sys/quest/" + qid + "/article.html",{credentials: "same-origin"}).then(function (response) {
                 if (response.status === 404)
                     console.log("");
                 if (response.status === 403)
@@ -13,7 +13,7 @@ function export_word(qid, filename) {
 }
 
 function export_txt(qid, filename) {
-    fetch("/resource/.sys/quest/" + qid + "/article.html").then(function (response) {
+    fetch("/resource/.sys/quest/" + qid + "/article.html",{credentials: "same-origin"}).then(function (response) {
                 if (response.status === 404)
                     console.log("");
                 if (response.status === 403)
@@ -25,6 +25,7 @@ function export_txt(qid, filename) {
                 raw_text = raw_text.replace(/<a .*?href=["']?([^"']*)["']?.*?>(.*)<\/a>/g, "$2 [$1]"); /* Convert links to something useful */
                 raw_text = raw_text.replace(/<(\/p|\/div|\/h\d|br)\w?\/?>/g,"\r\n"); /* Keep vertical whitespace intact. */
                 raw_text = raw_text.replace(/<[A-Za-z/][^<>]*>/g, ""); /* Remove the rest of the tags. */
+                raw_text = $('<textarea />').html(raw_text).text(); /* Convert HTML entities */
                 var txt = new Blob([raw_text], {type:"text/plain"});
                 saveAs(txt, filename+".txt");
             });
